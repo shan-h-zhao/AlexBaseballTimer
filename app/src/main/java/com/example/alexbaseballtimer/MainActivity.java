a98c2fd77a814f5aca72c032b16132e2430b8fcf
Shanshan Zhao <shanshanzhao@Shanshans-MacBook-Pro.local>
2021-11-17 14:59:10 -0500

    Done

--- app/src/main/java/com/example/alexbaseballtimer/MainActivity.java
+++ app/src/main/java/com/example/alexbaseballtimer/MainActivity.java
@@ -3,14 +3,27 @@ package com.example.alexbaseballtimer;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class MainActivity extends AppCompatActivity {
+
+    public String convertTime (Date milliSeconds) {
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss"); // HH for 0-23
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return df.format(milliSeconds);
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -24,31 +37,59 @@ public class MainActivity extends AppCompatActivity {
         seekBar.setMax(30);
         seekBar.setMin(3);
 
-        textView.setText(Integer.toString(seekBar.getProgress()));
+        Date d = new Date(seekBar.getProgress()* 1000L);
+        textView.setText(convertTime (d));
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 Log.i("value", Integer.toString(seekBar.getProgress()));
-                Integer seekBarProgress = seekBar.getProgress();
-                textView.setText(Integer.toString(seekBarProgress));
+                Date d = new Date(seekBar.getProgress()* 1000L);
+                textView.setText(convertTime (d));
 
-                new CountDownTimer(seekBarProgress*1000,1000) {
-
+                Button button= findViewById(R.id.button);
+                button.setOnClickListener(view -> new CountDownTimer(seekBar.getProgress()* 1000L,1000) {
                     @Override
                     public void onTick(long millisUntilFinished) {
-
-                        Log.i("seconds remaining: ", String.valueOf(millisUntilFinished / 1000));
-
+                        Date d1 = new Date(millisUntilFinished);
+                        textView.setText(convertTime (d1));
                     }
 
                     @Override
                     public void onFinish() {
+                        // Play sound
+                        MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.baseball);
+                        mediaPlayer.start();
 
+                        // Reset clock display
+                        Date d1 = new Date(seekBar.getProgress()* 1000L);
+                        textView.setText(convertTime (d1));
                     }
-                }.start();
-
+                }.start());
             }
+
+
+
+//                new CountDownTimer(seekBar.getProgress()*1000,1000) {
+//
+//                    @Override
+//                    public void onTick(long millisUntilFinished) {
+//                        Date d = new Date(millisUntilFinished);
+//                        textView.setText(convertTime (d));
+//                    }
+//
+//                    @Override
+//                    public void onFinish() {
+//
+//                        Date d = new Date(seekBar.getProgress()* 1000L);
+//                        textView.setText(convertTime (d));
+//
+//
+//
+//                    }
+//                }.start();
+
+
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
